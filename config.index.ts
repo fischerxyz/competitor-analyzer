50279504372c4f73014555577a70dfb8aa7ff96b
David Fischer <109357787+fischerxyz@users.noreply.github.com>
2024-01-07 15:02:59 +0100

    Update config.index.ts

--- config.index.ts
+++ config.index.ts
@@ -3,7 +3,7 @@ import {writeFile} from 'fs';
 const targetPath = './src/environments/environment.prod.ts';
 
 const envConfigFile = `export const environment = {
-    production: ${process.env.PRODUCTION},
+    production: true,
     OPENAI_APIKEY: '${process.env.OPENAI_APIKEY}',
     OPENAI_ORGANIZATION: '${process.env.OPENAI_ORGANIZATION}'
 };
